a0c337a582b850795977333958def5a95f0c5e6d
Unknown <goldfedertomer@gmail.com>
2019-06-09 15:51:10 +0300

    testing

--- test_sm.py
+++ test_sm.py
@@ -1,14 +1,30 @@
 import requests
 import sys
 import json
+import wapy_agent
+import time
+
 
 def main():
-    data = sys.argv[1]
+    # data = sys.argv[1]
+    #
+    # response = requests.post("http://localhost:8001/camera/{}".format(data), data=json.dumps({"mode": data}))
+    #
+    # print(response.status_code)
+    # print(response.text)
+    command = "python facial_landmarks.py"
 
-    response = requests.post("http://localhost:8001/camera/{}".format(data), data=json.dumps({"mode": data}))
+    out, errors = wapy_agent.execute_command(command)
 
-    print(response.status_code)
-    print(response.text)
+    time.sleep(10)
+
+    command = "ps aux | grep python"
+
+    out, error = wapy_agent.execute_command(command)
+
+    print(out)
+    print("\n\n")
+    print(error)
 
 
 if __name__ == "__main__":
